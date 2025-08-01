7b1a905cfa043b9fac229c4084daf255c469a501
Anubhab Dey (Transmogriffy Global) <transmogrify21@outlook.com>
2025-08-01 16:47:31 +0530

    Trynna fix

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [
@@ -10,6 +10,7 @@ export default defineConfig({
   server: {
     host: '127.0.0.1',   // listen on all network interfaces (LAN + localhost)
     port: 9090,        // explicitly specify port (default is 5173 anyway)
+	allowedHosts: ['hrms.transev.site'],
     proxy: {
       '/admin': {
         target: 'https://backend.hrms.transev.site',
